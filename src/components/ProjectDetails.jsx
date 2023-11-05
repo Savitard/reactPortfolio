4650e51e4243f434704854dc2c47f8bdc3d4e690
Savitard <c.lart@intech-sud.fr>
2023-11-05 16:03:02 +0100

    thème clair par défaut + système pour les détails projets + mat moi ca

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+// import ReactMarkdown from 'react-markdown';
+import { Container, Col } from 'react-bootstrap';
+import Header from './Header';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
 
 const styles = {
+  images: {
+    margin: 10,
+    display: 'flex',
+    justifyContent: 'center', // Pour centrer horizontalement
+    alignItems: 'center', // Pour centrer verticalement
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    border: '1px solid #ccc',
+    margin: '10px',
+  },
   introTextContainer: {
     margin: 10,
     flexDirection: 'column',
     whiteSpace: 'pre-wrap',
-    textAlign: 'left',
+    textAlign: 'justify',
     fontSize: '1.2em',
     fontWeight: 500,
   },
@@ -21,6 +37,7 @@ const styles = {
   },
   linkStyle: {
     margin: 30,
+    fontSize: 30,
   },
 };
 
@@ -35,18 +52,90 @@ function ProjectDetails() {
       .then((res) => setData(res))
       .catch((err) => err);
   }, []);
+
+  // const parseIntro = (text) => (
+  //   <ReactMarkdown
+  //     children={text}
+  //   />
+  // );
+
   const selectedProject = data?.projects.find((project) => project.title === title);
   return (
     <>
+      <Header title={selectedProject?.title} />
       {data
         ? (
           <div className="section-content-container">
             <Container style={styles.containerStyle}>
-              <p>{selectedProject.bodyText}</p>
-              <h2>Compétences associées</h2>
-              {selectedProject.tags.map((project) => (
-                <a href={`./#/skilldetails/${project.skill}`} style={styles.linkStyle}>{project}</a>
-              ))}
+              <div style={styles.contentContainer}>
+                <Col style={styles.introTextContainer}>
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].presentation }
+                  }
+                  />
+                  <br />
+                  <div style={styles.images}>
+                    <img src={`images/projects/${selectedProject.title}/1.jpg`} alt="1" style={{ maxWidth: '50%', height: 'auto' }} />
+                  </div>
+                  <br />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].objectifs }
+                  }
+                  />
+                  <br />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].etapes }
+                  }
+                  />
+                  <br />
+                  <div style={styles.images}>
+                    <img src={`images/projects/${selectedProject.title}/2.jpg`} alt="2" style={{ maxWidth: '50%', height: 'auto' }} />
+                    <img src={`images/projects/${selectedProject.title}/3.jpg`} alt="3" style={{ maxWidth: '50%', height: 'auto' }} />
+                  </div>
+                  <br />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].acteurs }
+                  }
+                  />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].resultats }
+                  }
+                  />
+                  <br />
+                  <div style={styles.images}>
+                    <img src={`images/projects/${selectedProject.title}/4.jpg`} alt="4" style={{ maxWidth: '50%', height: 'auto' }} />
+                  </div>
+                  <br />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].lendemain }
+                  }
+                  />
+                  <div dangerouslySetInnerHTML={
+                    { __html: selectedProject.details[0].critique }
+                  }
+                  />
+                </Col>
+                <h2 style={{ margin: '50px 0' }}>Compétences associées</h2>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {selectedProject.tags.map((tag) => (
+                    <div
+                      key={tag}
+                      style={{
+                        backgroundColor: '#00d084',
+                        color: 'white',
+                        borderRadius: '8px',
+                        padding: '8px 16px',
+                        margin: '0',
+                        display: 'inline-block',
+                        textDecoration: 'none',
+                        marginRight: '20px', // Marge à droite pour l'espacement
+                      }}
+                    >
+                      <a href={`./#/skilldetails/${tag}`} style={styles.linkStyle}>{tag}</a>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </Container>
           </div>
         ) : <FallbackSpinner />}
