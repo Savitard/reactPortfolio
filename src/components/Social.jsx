8f8fa33a695b97db4394dc93742bd9d2db4e1bfa
Savitard <corentin.lart@gmail.com>
2023-11-12 17:07:59 +0100

    bouton en savoir plus sur moi

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,14 +3,6 @@ import { SocialIcon } from 'react-social-icons';
 import { ThemeContext } from 'styled-components';
 import endpoints from '../constants/endpoints';
 
-const styles = {
-  iconStyle: {
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10,
-  },
-};
-
 function Social() {
   const theme = useContext(ThemeContext);
   const [data, setData] = useState(null);
@@ -24,20 +16,44 @@ function Social() {
       .catch((err) => err);
   }, []);
 
+  const styles = {
+    iconStyle: {
+      marginLeft: 10,
+      marginRight: 10,
+      marginBottom: 10,
+    },
+    btnStyle: {
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: theme.socialIconBgColor, // Use theme color
+      color: theme.socialIconColor, // Use theme color
+      padding: '10px',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      textDecoration: 'none',
+      marginLeft: '10px',
+      marginRight: '10px',
+      marginTop: '30px',
+    },
+  };
+
   return (
-    <div className="social">
-      {data ? data.social.map((social) => (
-        <SocialIcon
-          key={social.network}
-          style={styles.iconStyle}
-          url={social.href}
-          network={social.network}
-          bgColor={theme.socialIconBgColor}
-          target="_blank"
-          rel="noopener"
-        />
-      )) : null}
-    </div>
+    <>
+      <a href="#/about" style={styles.btnStyle}>En savoir plus sur moi</a>
+      <div className="social">
+        {data ? data.social.map((social) => (
+          <SocialIcon
+            key={social.network}
+            style={styles.iconStyle}
+            url={social.href}
+            network={social.network}
+            bgColor={theme.socialIconBgColor}
+            target="_blank"
+            rel="noopener"
+          />
+        )) : null}
+      </div>
+    </>
   );
 }
 
